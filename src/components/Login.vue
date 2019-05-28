8a04c84eccb620b356a77cd49b623c612896c989
max <max22012201@gmail.com>
2019-05-29 01:32:42 +0700

    front

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -21,38 +21,37 @@
   </div>
 </template>
 <script>
-  export default {
-    data(){
-      return {
-        email : "",
-        password : ""
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      if (this.password.length > 0) {
+        this.$http.post('http://localhost:3000/login', {
+          email: this.email,
+          password: this.password
+        })
+          .then(response => {
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+            localStorage.setItem('jwt', response.data.token)
+
+            if (localStorage.getItem('jwt') != null) {
+              this.$emit('loggedIn')
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl);
+              } else { this.$router.push('userboard'); }
+            }
+          })
+          .catch(function (error) {
+            console.error(error.response);
+          });
       }
     },
-    methods : {
-      handleSubmit(e){
-        e.preventDefault()
-        if (this.password.length > 0) {
-          this.$http.post('http://localhost:3000/login', {
-            email: this.email,
-            password: this.password
-          })
-            .then(response => {
-              localStorage.setItem('user',JSON.stringify(response.data.user))
-              localStorage.setItem('jwt',response.data.token)
-
-              if (localStorage.getItem('jwt') != null){
-                this.$emit('loggedIn')
-                if(this.$route.params.nextUrl != null){
-                  this.$router.push(this.$route.params.nextUrl)
-                }
-                else {this.$router.push('userboard')}
-              }
-            })
-            .catch(function (error) {
-              console.error(error.response);
-            });
-        }
-      }
-    }
-  }
+  },
+};
 </script>
