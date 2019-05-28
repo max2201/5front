ff28bc6f420afb6cf511bf657fe750fa557ae1d5
max <max22012201@gmail.com>
2019-05-29 00:51:48 +0700

    front

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,9 +2,9 @@
   <div>
     <h4>Register</h4>
     <form>
-      <label for="name">Name</label>
+      <label for="userName">Name</label>
       <div>
-        <input id="name" type="text" v-model="name" required autofocus>
+        <input id="userName" type="text" v-model="userName" required autofocus>
       </div>
 
       <label for="email" >E-Mail Address</label>
@@ -32,51 +32,49 @@
 </template>
 
 <script>
-  export default {
-    props : ["nextUrl"],
-    data(){
-      return {
-        name : "",
-        email : "",
-        password : "",
-        password_confirmation : "",
+export default {
+  props: ['nextUrl'],
+  data() {
+    return {
+      userName: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      if (this.password === this.password_confirmation && this.password.length > 0) {
+        const url = 'http://localhost:3000/auth/signup'
+        this.$http.post(url, {
+          userName: this.userName,
+          email: this.email,
+          password: this.password,
+        })
+          .then(response => {
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+            localStorage.setItem('jwt', response.data.token)
+
+            if (localStorage.getItem('jwt') != null) {
+              this.$emit('loggedIn')
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl);
+              }
+              else {
+                this.$router.push('/');
+              }
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        this.password = ''
+        this.passwordConfirm = ''
+        return alert('Passwords do not match');
       }
     },
-    methods : {
-      handleSubmit(e) {
-        e.preventDefault()
-
-        if (this.password === this.password_confirmation && this.password.length > 0)
-        {
-          let url = "http://localhost:3000/register"
-          this.$http.post(url, {
-            name: this.name,
-            email: this.email,
-            password: this.password,
-          })
-            .then(response => {
-              localStorage.setItem('user',JSON.stringify(response.data.user))
-              localStorage.setItem('jwt',response.data.token)
-
-              if (localStorage.getItem('jwt') != null){
-                this.$emit('loggedIn')
-                if(this.$route.params.nextUrl != null){
-                  this.$router.push(this.$route.params.nextUrl)
-                }
-                else{
-                  this.$router.push('/')
-                }
-              }
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        } else {
-          this.password = ""
-          this.passwordConfirm = ""
-          return alert("Passwords do not match")
-        }
-      }
-    }
-  }
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import Axios from 'axios';
 import App from './App.vue';
 import router from './router';
 
+
+Vue.prototype.$http = Axios;
 Vue.config.productionTip = false;
 
 new Vue({
